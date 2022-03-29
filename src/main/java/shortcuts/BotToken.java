bc9b06b8a952f38b5a918047bd37e2f3a4c0e08a
SkyExit <georgkoltwig@gmail.com>
2022-03-29 21:18:42 +0200

    v1.10.167

--- src/main/java/shortcuts/BotToken.java
+++ src/main/java/shortcuts/BotToken.java
@@ -2,6 +2,6 @@ package shortcuts;
 
 public class BotToken {
     public static String getBotToken() {
-        return "ODgxODU2OTIyNDEwNzAwODMw.YSy7Qg.gITxuvojLxNem1itcMqt6wNo-B8";
+        return "sdasdw";
     }
 }
